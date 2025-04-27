a2626e76d0bab7db1a7e079353e09a5c7bb8c0b7
Valentino Junior <ojvalentine14@gmail.com>
2025-04-27 11:47:42 +0300

    Added third part apps and local apps

--- health_info_system/settings.py
+++ health_info_system/settings.py
@@ -37,6 +37,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third party apps
+    'rest_framework',
+    'django_filters',
+    'crispy_forms',
+    'crispy_bootstrap5',
+    
+    # Local apps
+    'core',
+    'api',
 ]
 
 MIDDLEWARE = [
