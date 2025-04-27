d87a37009b49a484b0d8d2db55e7f01e609ceeda
Valentino Junior <ojvalentine14@gmail.com>
2025-04-27 11:49:53 +0300

    Allowed cripsy forms

--- health_info_system/settings.py
+++ health_info_system/settings.py
@@ -65,6 +65,11 @@ REST_FRAMEWORK = {
 }
 
 
+# Crispy Forms
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
