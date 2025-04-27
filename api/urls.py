0480625275bdec218b9da978d14d06197e5f5e12
Valentino Junior <ojvalentine14@gmail.com>
2025-04-27 12:50:25 +0300

    Updated project url

---
+++ api/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from . import views
+
+# Create a router and register our viewsets with it
+router = DefaultRouter()
+router.register(r'clients', views.ClientViewSet)
+router.register(r'programs', views.HealthProgramViewSet)
+
+# The API URLs are now determined automatically by the router
+urlpatterns = [
+    path('', include(router.urls)),
+    path('auth/', include('rest_framework.urls', namespace='rest_framework')),
+]
