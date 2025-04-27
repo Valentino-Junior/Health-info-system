7539d7f539fa8a0c851669bc9a660d7cd2fb7ecd
Valentino Junior <ojvalentine14@gmail.com>
2025-04-27 12:30:05 +0300

    Added healthprogramm  url

--- core/urls.py
+++ core/urls.py
@@ -4,3 +4,10 @@ from . import views
 urlpatterns = [
     # Dashboard
     path('', views.dashboard, name='dashboard'),
+
+
+    # Health Program URLs
+    path('programs/', views.program_list, name='program_list'),
+    path('programs/<uuid:pk>/', views.program_detail, name='program_detail'),
+    path('programs/create/', views.program_create, name='program_create'),
+    path('programs/<uuid:pk>/update/', views.program_update, name='program_update'),
