d673b84fd54c95f0c4289a92d7a6a96fe2568581
Valentino Junior <ojvalentine14@gmail.com>
2025-04-27 12:21:50 +0300

    Created health program model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils import timezone
+import uuid
 
-# Create your models here.
+
+class HealthProgram(models.Model):
+    """Model representing a health program (TB, Malaria, HIV, etc.)"""
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        ordering = ['name']
+    
+    def __str__(self):
+        return self.name
