5583d9b2c6769ebb7242f21f5e8141e3f9ba24b6
Valentino Junior <ojvalentine14@gmail.com>
2025-04-27 11:49:16 +0300

    Added restframework settings

--- health_info_system/settings.py
+++ health_info_system/settings.py
@@ -49,6 +49,22 @@ INSTALLED_APPS = [
     'api',
 ]
 
+
+# Rest Framework settings
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.SearchFilter',
+        'rest_framework.filters.OrderingFilter',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
